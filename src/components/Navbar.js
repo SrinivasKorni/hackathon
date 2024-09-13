46f5baf8572a1c114b86d56b8684c5f1aa27fa69
SrinivasKorni <21bd1a05aqcsef@gmail.com>
2024-09-13 09:00:39 +0530

    empty committed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
-import "./Navbar.css"; // Import the CSS file
+import "./Navbar.css";
 
 function Navb() {
   return (
